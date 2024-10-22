8bbd0bbfb5368804448ad6649c303e91f0b63275
xiaoran <xiaoran2495897824@163.com>
2024-10-23 01:01:14 +0800

    Update: new config and shell stage 3

--- run_stage3.sh
+++ run_stage3.sh
@@ -1,3 +1,3 @@
 ##### PLEASE UPDTAE CONFIGURE FILE BEFORE EXCUTING THIS SCRIPT #####
-python3 train_diff_model.py -p train -c config/hardi_150_r.json
+python3 train_diff_model.py -p train -c config/hardi_150_phase3.json
 ##### PLEASE UPDTAE CONFIGURE FILE BEFORE EXCUTING THIS SCRIPT #####
